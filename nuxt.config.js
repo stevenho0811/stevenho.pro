ceb56d622780ccaff91f109a486c55b2c1cb9bfd
Steven Ho <shockshocksoul@gmail.com>
2021-03-29 00:54:12 +0800

    feat(info): add basic info

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,14 +17,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ['~/assets/scss/app.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: '~/plugins/gtag', mode: 'client' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -51,7 +50,7 @@ export default {
     display: 'swap',
     families: {
       Inter: {
-        wght: [400, 900],
+        wght: [400, 700, 900],
       },
     },
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,82 @@
 <template>
-  <div class="h-screen text-white w-full lg:w-1/2 mx-auto">
-    <div class="grid place-content-center h-full w-full px-12">
-      <Logo class="w-28 h-28 mb-12" />
-      <h1 class="font-black text-8xl tracking-wide uppercase leading-normal">
-        Steven Ho
-      </h1>
+  <div class="h-screen text-white w-full lg:w-3/4 xl:w-1/2 mx-auto">
+    <div class="grid place-content-center h-full w-full px-8 lg:px-0">
+      <Logo
+        ref="logo"
+        class="w-20 h-20 lg:w-28 lg:h-28 mb-8 mx-auto lg:mx-0 invisible"
+      />
+      <div class="leading-normal text-center lg:text-left">
+        <div class="overflow-hidden">
+          <h1
+            ref="name"
+            class="font-black text-4xl lg:text-8xl tracking-wide uppercase transform-gpu translate-y-full invisible"
+          >
+            Steven Ho
+          </h1>
+        </div>
+
+        <div class="overflow-hidden">
+          <h2
+            ref="title"
+            class="font-bold transform-gpu translate-y-full invisible pt-2"
+          >
+            Web Developer
+          </h2>
+        </div>
+        <p ref="description" class="my-6 invisible">
+          Use Nuxt.js(Vue) and other Javascript framework to build Modern
+          Website, extra skill includes JS backend with MongoDB, UI/UX Design.
+        </p>
+
+        <div
+          ref="contact"
+          class="text-blue-600 text-base grid auto-rows-max gap-2 invisible"
+        >
+          <a
+            class="hover:text-blue-400 transition-colors"
+            href="mailto:hi@stevenho.pro"
+            >hi@stevenho.pro</a
+          >
+          <a
+            class="hover:text-blue-400 transition-colors"
+            href="tel:+886908930811"
+            >+886-908-930-811</a
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { gsap } from 'gsap'
 import Logo from '~/assets/svg/logo.svg?inline'
+
 export default {
   components: {
     Logo,
+  },
+  mounted() {
+    const timeline = gsap.timeline({
+      defaults: {
+        ease: 'expo',
+        duration: 0.75,
+      },
+    })
+
+    timeline
+      .to(this.$refs.logo, {
+        autoAlpha: 1,
+      })
+      .to([this.$refs.name, this.$refs.title], {
+        stagger: 1,
+        yPercent: -100,
+        autoAlpha: 1,
+      })
+      .to([this.$refs.description, this.$refs.contact], {
+        stagger: 1,
+        autoAlpha: 1,
+      })
   },
 }
 </script>
