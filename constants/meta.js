c987dbc9c7f6ea2814df0ed26d5b964db8ac7dc9
Steven Ho <shockshocksoul@gmail.com>
2021-09-06 18:33:53 +0800

    chore: remove open graph url from meta

--- constants/meta.js
+++ constants/meta.js
@@ -8,5 +8,4 @@ export default {
     'Use Nuxt.js(Vue) and other Javascript framework to build Modern Website, extra skill includes JS backend with MongoDB, UI/UX Design.',
   theme_color: '#151515',
   ogImage: 'https://stevenho.pro/cover.jpg',
-  ogUrl: 'https://stevenho.pro',
 }
